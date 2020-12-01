383139209f583fe36cebea8500c51d8c25e97be2
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2020-12-01 12:38:28 +0100

    Added fix for preflight request

--- client/control.js
+++ client/control.js
@@ -20,3 +20,27 @@ function onget(version) {
         document.getElementById('messages').value = error;
     });
 }
+
+function onput(version) {
+    const url = "http://localhost:8000/api/" + version + "/messages/0";
+    var headers = {}
+
+    fetch(url, {
+        method : "PUT",
+        mode: 'cors',
+        headers: headers,
+        body: new URLSearchParams(new FormData(document.getElementById("form2"))),
+    })
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.error)
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.getElementById('messages').value = data.messages;
+    })
+    .catch(function(error) {
+        document.getElementById('messages').value = error;
+    });
+}
--- cors/server.go
+++ cors/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,9 +17,24 @@ func GetMessages(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"messages": messages})
 }
 
+func PutMessage(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	text := c.PostForm("puttext")
+	messages[id] = text
+	c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+	c.JSON(http.StatusOK, gin.H{"messages": messages})
+}
+
+func OptionMessage(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+	c.Header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
+}
+
 func main() {
 	messages = append(messages, "Hello CORS!")
 	r := gin.Default()
 	r.GET("/api/:version/messages", GetMessages)
+	r.PUT("/api/:version/messages/:id", PutMessage)
+	r.OPTIONS("/api/v2/messages/:id", OptionMessage)
 	r.Run(":8000")
 }
